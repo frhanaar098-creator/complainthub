01f600646b8065f94c117e2c6a64c16e2f486774
farhana <frhana.ar098@gmail.com>
2026-02-14 08:59:23 +0530

    change:base url and the cors

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,17 @@ const __dirname = path.dirname(__filename);
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+      origin: [
+        'https://complainthub-ten.vercel.app',
+        'http://localhost:3000'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true
+    })
+  );
+  
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const AuthContext = createContext(null);
 
-const API_URL = '/api';
+const API_URL = 'https://complainthub-9ly4.onrender.com/';
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
